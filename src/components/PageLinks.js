d8169a9fd5ea3b5a07ebd2a7243cba329ca5cc1a
Antoine Randle <randleantoine@gmail.com>
2023-06-27 17:28:54 -0600

    More components

--- src/components/PageLinks.js
+++ src/components/PageLinks.js
@@ -1,4 +1,5 @@
 import { pageLinks } from '../data';
+import PageLink from './PageLink';
 
 // You can split it up into more components. It's definitely an option. The result will be exactly the same. Just be aware of the trade offs in this case. If you use component for entire list, you will have to set up and pass the props.
 
@@ -6,13 +7,7 @@ const PageLinks = ({ parentClass, itemClass }) => {
   return (
     <ul className={parentClass} id='nav-links'>
       {pageLinks.map((link) => {
-        return (
-          <li key={link.id}>
-            <a href={link.href} className={itemClass}>
-              {link.text}
-            </a>
-          </li>
-        );
+        return <PageLink key={link.id} link={link} itemClass={itemClass} />;
       })}
     </ul>
   );
